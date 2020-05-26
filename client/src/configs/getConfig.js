937a1d550199495c92e44e48539bdda9ffd919fb
Aspyn Palatnick <aspyn@seas.upenn.edu>
2020-05-26 13:42:24 -0600

    fix comment

--- client/src/configs/getConfig.js
+++ client/src/configs/getConfig.js
@@ -1,7 +1,6 @@
 /**
  * Get the config for the current build type, which
- * ensures that only the given config is included in the
- * build, which will prevent info/api key leakage
+ * ensures that only the given config is included in the build
  */
 export default function getConfig() {
   return process.env.REACT_APP_BUILD_TYPE === "development"
